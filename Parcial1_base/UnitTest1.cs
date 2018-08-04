cf91f7560ba027b5066e519a266e42c6f9324d24
DARK_ETERNAL <tomailsandstuff@gmail.com>
2018-08-04 16:29:57 -0500

    Add project files.

---
+++ Parcial1_base/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Parcial1_base
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
